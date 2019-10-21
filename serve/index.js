ca1799671d5ae0ee8c984cac83ec76c4f12a40ae
despossivel <mattbmoller@gmail.com>
2019-10-21 20:40:58 -0300

    refatorando

--- serve/index.js
+++ serve/index.js
@@ -1,36 +1,14 @@
 var express = require('express')
-    , app = express()
-    , server = require('http').createServer(app).listen(21041)
-    , io = require('socket.io').listen(server)
-    , ss = require('socket.io-stream')
-    , path   = require('path')
-    , fs = require('fs')
-    , port = process.env.PORT || 8080 
-      router = express.Router();
+  , app = express()
+  , server = require('http').createServer(app).listen(21041)
+  , io = require('socket.io').listen(server)
+  , ss = require('socket.io-stream')
+  , path = require('path')
+  , fs = require('fs')
+  , port = process.env.PORT || 8080
+router = express.Router();
 
-    app.listen(port);
-	    console.log('conectado a 21041 ');
+app.listen(port);
+console.log('conectado a 21041 ');
 
-
-io.on('connection', function(socket) {
- 
-  ss(socket).on('file', function(stream,data) {
-    var filename = path.basename(data.name);
-
- 	  stream.pipe(fs.createWriteStream('./uploads/'+filename));
-
-
-
-
-
-
-
-
-
-
-  });
-
-
-
-
-});
+io.on('connection', (socket) => ss(socket).on('file', (stream, data) => stream.pipe(fs.createWriteStream('./uploads/' + path.basename(data.name)))));
